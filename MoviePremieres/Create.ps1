bf3bf4b4314be1efc9ef5617367e70f73d39b921
amularczyk <xniqux@gmail.com>
2018-09-20 06:56:54 +0200

    Working on power-shell script to create azure storage and table.

--- MoviePremieres/Create.ps1
+++ MoviePremieres/Create.ps1
@@ -5,17 +5,32 @@ param (
 	[string]$password,
 	[string]$serverName,
 	[string]$databaseName,
-	[string]$ip
+	[string]$ip,
+	[string]$storageAccountName,
+	[string]$tableName	
 )
 
 Import-Module AzureRM
 
 Connect-AzureRmAccount
 
+#Resource Group
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 
-New-AzureRmSqlServer -ResourceGroupName $resourceGroup -Location $location -ServerName $serverName -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $login, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
 
-New-AzureRmSqlServerFirewallRule -ResourceGroupName $resourceGroup -ServerName $serverName -FirewallRuleName "AlloweMe" -StartIpAddress $ip -EndIpAddress $ip
+#Sql Server
+#New-AzureRmSqlServer -ResourceGroupName $resourceGroup -Location $location -ServerName $serverName -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $login, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
 
-New-AzureRmSqlDatabase  -ResourceGroupName $resourceGroup -ServerName $serverName -DatabaseName $databaseName -Edition "Basic"
+#Sql Server Firewall Rule
+#New-AzureRmSqlServerFirewallRule -ResourceGroupName $resourceGroup -ServerName $serverName -FirewallRuleName "AlloweMe" -StartIpAddress $ip -EndIpAddress $ip
+
+#Sql Database
+#New-AzureRmSqlDatabase  -ResourceGroupName $resourceGroup -ServerName $serverName -DatabaseName $databaseName -Edition "Basic"
+
+
+#Sql Storage
+$storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroup -Name $storageAccountName -Location $location -SkuName Standard_LRS -Kind StorageV2 -AccessTier Hot
+$ctx = $storageAccount.Context
+
+#Sql Sotrage Table
+New-AzureStorageTable –Name $tableName –Context $ctx
